380af93da021dd2c8aee7b528b57de403230948e
Adrian Furgała <adrian.furgala@softiq.pl>
2019-10-02 15:24:22 +0200

    dyrektywa highlight

---
+++ src/app/shared/directives/highlight.directive.ts
@@ -0,0 +1,26 @@
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  @HostListener('mouseover') mouseOverHandler() {
+    console.log('mouseover');
+    const $element = this.element.nativeElement;
+    this.renderer.addClass($element, 'border');
+    this.renderer.addClass($element, 'border-primary');
+  }
+
+  @HostListener('mouseleave') mouseLeaveHandler() {
+    console.log('mouseleave');
+    const $element = this.element.nativeElement;
+    this.renderer.removeClass($element, 'border');
+    this.renderer.removeClass($element, 'border-primary');
+  }
+
+  constructor(private element: ElementRef,
+              private renderer: Renderer2) {
+  }
+
+}
